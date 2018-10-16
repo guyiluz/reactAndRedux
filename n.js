2df951aeb4e716f62887b65afe4196a003098be5
guyiluz <iloz.guy@gmail.com>
2018-10-16 11:05:52 +0300

    fix git bug

--- n.js
+++ n.js
@@ -1,1 +1,26 @@
-console.log(this)
+const i = "01:00"
+
+
+
+const  stringToNumber= (string)=>string.split("").filter(i=>isNaN(i,)==false).map(i=>Number(i))
+
+const getMilisecFromString=(numString)=>{
+  stringToNumbersArry= stringToNumber(numString)
+      const MiliscCounterObj={
+        "0": (num) => num !== 0 ? num * 10 * 60 * 1000 : 0,
+        "1": (num) => num !== 0 ? num * 60 * 1000 : 0,
+        "2": (num) => num !== 0 ? num  * 10 * 1000 : 0,
+        "3": (num) => num !== 0 ? num * 1000 : 0,
+  
+       
+           }
+   
+  return  stringToNumbersArry.reduce((accumulator, currentValue, currentIndex)=> accumulator + MiliscCounterObj[currentIndex](currentValue)) 
+
+}
+
+// var y =stringToNumber(i)
+
+ var y =getMilisecFromString(i)
+
+console.log(y);
--- src/containers/CountDown.js
+++ src/containers/CountDown.js
@@ -1,239 +1,214 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import CheckboxGroup from '../../node_modules/antd/lib/checkbox/Group';
-import {connect} from "react-redux";
-import {SET_TIMER} from "../actions/items";
-var audio = new Audio(require('./Meditation-bell-sound.mp3') );
+import { connect } from "react-redux";
+import { SET_TIMER } from "../actions/items";
+var audio = new Audio(require('./Meditation-bell-sound.mp3'));
 
 
 
 class CountDown extends Component {
-  constructor(props){
-  
+  constructor(props) {
+
     super(props)
-    const timerName =this.props.timerName
-    const time= this.props.timers[timerName]
-    this.state={
-   mBig:parseInt(time[0]) ,
-   mSmall:parseInt(time[1]),
-   sBig:parseInt(time[3]),
-   sSmall:parseInt(time[4]),
-   isMouseUp:false,
-   arrowBtn:"",
-   counter:0,
-   totalTime:0,
-   timerName:"",
-   status:"",
-   intervalNum:"",
-   intervalId:""
+    const timerName = this.props.timerName
+    const time = this.props.timers[timerName]
+    this.state = {
+      mBig: parseInt(time[0]),
+      mSmall: parseInt(time[1]),
+      sBig: parseInt(time[3]),
+      sSmall: parseInt(time[4]),
+      isMouseUp: false,
+      arrowBtn: "",
+      counter: 0,
+      totalTime: 0,
+      timerName: "",
+      status: "",
+      intervalNum: "",
+      intervalId: ""
 
 
-  
-   
+
+
 
 
 
     }
   }
 
+  stringToNumber = (string) => string.split("").filter(i => isNaN(i) == false).map(i => Number(i))
 
+  getMilisecFromString = (numString) => {
+   const stringToNumbersArry = this.stringToNumber(numString)
+    const MiliscCounterObj = {
+      "0": (num) => num !== 0 ? num * 10 * 60 * 1000 : 0,
+      "1": (num) => num !== 0 ? num * 60 * 1000 : 0,
+      "2": (num) => num !== 0 ? num  * 10 * 1000 : 0,
+      "3": (num) => num !== 0 ? num * 1000 : 0,
 
-  getMilisecFromString =(timeObj)=>{
-
-    let intervalObj ={
-      mBig:Number(timeObj[0]),
-      mSmall:Number(timeObj[1]),
-      sBig:Number(timeObj[3]),
-      sSmall:Number(timeObj[4]),
     }
 
 
 
+    return stringToNumbersArry.reduce((accumulator, currentValue, currentIndex) => accumulator + MiliscCounterObj[currentIndex](currentValue))
 
-   let millis =0
-    let {mBig,mSmall,sBig,sSmall}=intervalObj
-    let  mBigNromlise=mBig!==0?mBig*10*60*1000:0
-    let  mSmallNromlise=mSmall!==0?mSmall*60*1000:0
-    let sBigNromlise=sBig!==0?sBig*10*1000:0
-    let sSmallNromlise=sSmall!==0?sSmall*1000:0
-    millis=  mBigNromlise+mSmallNromlise+sBigNromlise+sSmallNromlise
-
- return millis
   }
 
 
 
-  activeIntrevalBell=(intervvalTime,millis)=>{
-    console.log("activeIntrevalBell:",millis);
 
+
+
+
+
+  HandleStartTimer = () => {
+
+    const { timers } = this.props
+    const { status, totalTime } = this.state
+    const { medation, interval } = timers
+    const   intervvalTime = this.getMilisecFromString(interval)
+    const medationTime = this.getMilisecFromString(medation)
+
+   let counter =0
+
+
+    this.millis = 0
+
+
+    const millisToMinutesAndSeconds = () => {
+      const { status, totalTime } = this.state
+      const { medation, interval } = timers
+console.log('object');
+console.log('intervvalTime :', intervvalTime);
+console.log('counter :', counter);
+if(intervvalTime!==counter&&status=="medation"){
+  counter =counter+1000
+
+
+}else {
+  counter=0
   
 
-    this.setState({intervalId:setInterval(()=>{
-      audio.pause()
-  
-      audio.play();
-  
-     
-   console.log("ITERVAL ON ")
-    },intervvalTime)
-
-  
-  
-  })
+}
 
 
+      if (this.millis !== 0) {
+        this.millis = this.millis - 1000
+        setTimeout(() => {
+          var minutes = Math.floor(this.millis / 60000);
+          var seconds = ((this.millis % 60000) / 1000).toFixed(0);
+          let stringTime = minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+          let totalTime = minutes + seconds
+          if (stringTime.length < 5) {
+            stringTime = "0" + stringTime
 
-  } 
+          }
 
-  HandleStartTimer=()=>{
+          console.log(this.millis)
 
-    let intervvalTime;
-    const {timers}= this.props
-  const {status,totalTime}=this.state
-
-  let interval=timers.interval;
-  let intervalNum=0;
+          this.setState({
+            mBig: Number(stringTime[0]),
+            mSmall: Number(stringTime[1]),
+            sBig: Number(stringTime[3]),
+            sSmall: Number(stringTime[4]),
+            totalTime
 
 
 
-    let millis=0
 
-    const millisToMinutesAndSeconds=( )=> {
-
-      if(millis!==0){  
-      millis=millis-1000
-      setTimeout(()=>{
-        var minutes = Math.floor(millis / 60000);
-        var seconds = ((millis % 60000) / 1000).toFixed(0);
-       let stringTime=  minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-       let totalTime =minutes+seconds
- if(stringTime.length<5){
-  stringTime = "0"+stringTime
-
- }
+          })
 
 
-       this.setState({
-        mBig:Number(stringTime[0]),
-        mSmall:Number(stringTime[1]),
-        sBig:Number(stringTime[3]),
-        sSmall:Number(stringTime[4]),
-        totalTime
-  
+          if (totalTime == "01" && this.state.status === "Get Ready" && this.state.status !== "medation") {
+            audio.play();
+
+            if (timers.interval == "00:00") {
+              const { medation, interval } = timers
+              this.millis = this.getMilisecFromString(medation)
+
+              this.setState({
+                status: "medation"
+              })
 
 
-
-      })
-       if(totalTime=="01"&&this.state.status==="Get Ready"&&this.state.status!=="medation"){
-        audio.pause()
-
-        audio.play();
-        if(timers.interval=="00:00"){
-          const {medation,interval} =timers
-                  millis=  this.getMilisecFromString(medation)
+            } else {
+              console.log(this.getMilisecFromString)
           
-                  this.setState({
-                    status:"medation"
-                   })
+              console.log('intervvalTime :', intervvalTime);
+              console.log('intervvalTime :', medationTime);
 
 
-        }else {
-          const {medation,interval} =timers
-   intervvalTime = this.getMilisecFromString(interval)
-  let medationTime= this.getMilisecFromString(medation)
-  millis =medationTime
-
-this.setState({status:"medation"})
+              this.setState({
+                status: "medation"
+              })
+              this.millis = medationTime
 
 
 
 
-if(intervvalTime>0){
-  
-  setTimeout(() => {
-    console.log("xxxxxxxxxxxxxxxx");
-  }, intervvalTime);
-  
-  this.activeIntrevalBell(intervvalTime,millis)
+            }
+          }
 
-}
-
-
-
-        }
-        }
-  
-      millisToMinutesAndSeconds(millis) 
+          millisToMinutesAndSeconds(this.millis)
 
 
 
 
-  
-     },1000)
-    }else{
 
- 
-        console.log("time is ended");
-    clearInterval(this.state.intervalId)
-    
-    
-          
-      
-    }
+        }, 1000)
+      }
 
     }
 
 
-    let {mBig,mSmall,sBig,sSmall}=this.state
-  let  mBigNromlise=mBig!==0?mBig*10*60*1000:0
-  let  mSmallNromlise=mSmall!==0?mSmall*60*1000:0
-  let sBigNromlise=sBig!==0?sBig*10*1000:0
-  let sSmallNromlise=sSmall!==0?sSmall*1000:0
+    let { mBig, mSmall, sBig, sSmall } = this.state
+    let mBigNromlise = mBig !== 0 ? mBig * 10 * 60 * 1000 : 0
+    let mSmallNromlise = mSmall !== 0 ? mSmall * 60 * 1000 : 0
+    let sBigNromlise = sBig !== 0 ? sBig * 10 * 1000 : 0
+    let sSmallNromlise = sSmall !== 0 ? sSmall * 1000 : 0
 
-  millis=  mBigNromlise+mSmallNromlise+sBigNromlise+sSmallNromlise
-  millisToMinutesAndSeconds(millis)
+    this.millis = mBigNromlise + mSmallNromlise + sBigNromlise + sSmallNromlise
+    millisToMinutesAndSeconds(this.millis)
 
   }
-  
 
 
-componentWillUnmount(){
 
-clearInterval(this.state.intervalId)
-
-
-}
-
-  componentDidMount(){
- const {timers}= this.props
-if(timers.prepare!=="00:00"){
- this.setState({
-  mBig:parseInt(timers.prepare[0]) ,
-  mSmall:parseInt(timers.prepare[1]),
-  sBig:parseInt(timers.prepare[3]),
-  sSmall:parseInt(timers.prepare[4]),
-  status:"Get Ready",
-  totalTime:timers.prepare
+  componentWillUnmount() {
+    clearInterval(this.timer)
 
 
- },()=>{
+  }
 
-this.HandleStartTimer()
+  componentDidMount() {
+    const { timers } = this.props
+    if (timers.prepare !== "00:00") {
+      this.setState({
+        mBig: parseInt(timers.prepare[0]),
+        mSmall: parseInt(timers.prepare[1]),
+        sBig: parseInt(timers.prepare[3]),
+        sSmall: parseInt(timers.prepare[4]),
+        status: "Get Ready",
+        totalTime: timers.prepare
 
- }
- 
- 
- )
+
+      }, () => {
+
+        this.HandleStartTimer()
+
+      }
 
 
-}
-     
+      )
+
+
+    }
+
 
 
 
 
   }
 
- 
 
 
 
@@ -242,7 +217,8 @@ this.HandleStartTimer()
 
 
 
- 
+
+
 
 
 
@@ -252,94 +228,94 @@ this.HandleStartTimer()
 
 
   render() {
-  
-    const {mBig,mSmall,sBig,sSmall}=this.state
 
-     
-
-
-
-
-const testStyle={
-  fontSize: "9rem",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  height: "100%",
-
-
-}
-
-const timerStyle={
-    color: "rgb(182, 198, 222)",
-    fontSize: "12rem",
-    display: "flex",
-    
-
-
-}
-
-const NumberStyles={
-    alignSelf: "center",
-    "lineHeight":"0.5",
-}
+    const { mBig, mSmall, sBig, sSmall } = this.state
 
 
 
 
 
-const numberDivStyle ={
 
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
+    const testStyle = {
+      fontSize: "9rem",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100%",
+
+
+    }
+
+    const timerStyle = {
+      color: "rgb(182, 198, 222)",
+      fontSize: "12rem",
+      display: "flex",
+
+
+
+    }
+
+    const NumberStyles = {
+      alignSelf: "center",
+      "lineHeight": "0.5",
+    }
 
 
 
 
-}
+
+    const numberDivStyle = {
+
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+
+
+
+
+    }
 
 
 
     return (
- 
-
-
-  
-<div>
-
-
- 
- <div style={timerStyle}>
-<div style={numberDivStyle} > 
-
-<div style={NumberStyles}  >{mBig}</div>
-
-
-</div>
-<div style={NumberStyles} > {mSmall}</div>
-<div style={NumberStyles} > :</div>
-<div  style={numberDivStyle}  > 
-
-<div style={NumberStyles} >{sBig}</div>
-
-</div>
-<p style={NumberStyles}>{sSmall}</p>
-
-  </div>
-
-<div>
-<p>{this.state.status}</p>
-<p>{this.state.intervalNum}</p>
-
-</div>
 
 
 
-  </div>
-       
-    
+
+      <div className="CountDown">
+
+
+
+        <div style={timerStyle}>
+          <div style={numberDivStyle} >
+
+            <div style={NumberStyles}  >{mBig}</div>
+
+
+          </div>
+          <div style={NumberStyles} > {mSmall}</div>
+          <div style={NumberStyles} > :</div>
+          <div style={numberDivStyle}  >
+
+            <div style={NumberStyles} >{sBig}</div>
+
+          </div>
+          <p style={NumberStyles}>{sSmall}</p>
+
+        </div>
+
+        <div>
+          {/* <p>{this.state.status}1111111111111111</p>
+<p>{this.state.intervalNum}222222222222222</p> */}
+
+        </div>
+
+
+
+      </div>
+
+
     )
   }
 }
@@ -348,8 +324,10 @@ const numberDivStyle ={
 
 const mapStateToProps = (state) => {
   return {
-    timers:state.timers,
-    timerName:state.timerName      
+    timers: state.timers,
+    timerName: state.timerName,
+    modes: state.modes
+
 
 
   };
@@ -359,8 +337,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    SET_TIMER:(obj)=>dispatch(SET_TIMER(obj))
-    
+    SET_TIMER: (obj) => dispatch(SET_TIMER(obj))
+
 
   };
 };
@@ -375,7 +353,6 @@ export default connect(mapStateToProps, mapDispatchToProps)(CountDown);
 
 
 
-  
 
-   
-   
+
+
